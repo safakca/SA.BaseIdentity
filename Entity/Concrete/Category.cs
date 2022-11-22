5cf0866c2470719760d220ed46d2e80491b1abb9
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2022-11-22 22:35:37 +0300

    Add project files.

---
+++ Entity/Concrete/Category.cs
@@ -0,0 +1,12 @@
+﻿
+using Entity.Common;
+using System.Collections.Generic;
+
+namespace Entity.Concrete
+{
+    public class Category : BaseEntity
+    {
+        public int Name { get; set; }
+        public virtual ICollection<Category> Categories{ get; set; }
+    }
+}
---
+++ Entity/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿
+using Entity.Common;
+
+namespace Entity.Concrete
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock{ get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
